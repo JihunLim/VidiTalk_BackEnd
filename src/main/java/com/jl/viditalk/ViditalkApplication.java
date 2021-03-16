768599ef0e24064383a60497918ddd833591b49e
jihunLim <limjihun204@naver.com>
2021-03-17 01:49:10 +0900

    First Commit - VidiTalk Sprint RestAPI Server

---
+++ src/main/java/com/jl/viditalk/ViditalkApplication.java
@@ -0,0 +1,13 @@
+package com.jl.viditalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViditalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViditalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jl/viditalk/ViditalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jl.viditalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViditalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
